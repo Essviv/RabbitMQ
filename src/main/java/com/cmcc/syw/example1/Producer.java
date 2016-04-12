9d5b9de9ee7d2a84ddc417a7509ed6b8d4d55b15
essviv <514912821@qq.com>
2016-04-12 19:14:54 +0800

    add product implement code

--- src/main/java/com/cmcc/syw/example1/Producer.java
+++ src/main/java/com/cmcc/syw/example1/Producer.java
@@ -5,30 +5,47 @@ import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeoutException;
 
 /**
  * Created by sunyiwei on 16-4-5.
  */
 public class Producer {
-    private final static String QUEUE_NAME = "HELLO";
+    private final static String QUEUE_NAME = "no.ha.all";
 
     public static void main(String[] args) throws IOException, TimeoutException {
         ConnectionFactory connectionFactory
                 = new ConnectionFactory();
-        connectionFactory.setHost("localhost");
+        connectionFactory.setHost("192.168.32.68");
         connectionFactory.setPort(5672);
+        connectionFactory.setUsername("sunyiwei");
+        connectionFactory.setPassword("syw514912821");
 
-        Connection connection
+        final Connection connection
                 = connectionFactory.newConnection();
-        Channel channel = connection.createChannel();
-        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
 
-        String message = "hello world";
-        channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
-        System.out.println("[X] Send: " + message);
+        final int COUNT = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(COUNT);
 
-        channel.close();
-        connection.close();
+        for (int i = 0; i < COUNT; i++) {
+            executorService.submit(new Runnable() {
+                public void run() {
+                    try {
+                        Channel channel = connection.createChannel();
+                        channel.queueDeclare(QUEUE_NAME, true, false, false, null);
+
+                        String message = "hello world";
+
+                        while (true) {
+                            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
     }
 }
