e6a3efe4be3adffae90dc632b6cd8a08923c6829
essviv <514912821@qq.com>
2016-09-11 10:38:32 +0800

    add publish ack confirm code

--- src/main/java/com/cmcc/syw/PublishAck.java
+++ src/main/java/com/cmcc/syw/PublishAck.java
@@ -6,29 +6,33 @@ import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
 import java.io.IOException;
+import java.util.SortedSet;
+import java.util.TreeSet;
 import java.util.concurrent.TimeoutException;
 
 /**
- * demo of acknowledgement of publish
- * <p/>
- * Created by sunyiwei on 16/4/9.
+ * demo of acknowledgement of publish <p/> Created by sunyiwei on 16/4/9.
  */
 public class PublishAck {
     private static final String HOST = "localhost";
     private static final String EXCHANGE = "DIRECT";
+    private static final String USERNAME = "guest";
+    private static final String PASSWORD = "guest";
     private static final String QUEUE = "EXCLUSIVE_QUEUE";
     private static final int PORT = 5672;
     private static final int COUNT = 100000;
 
-    public static void main(String[] args) throws IOException, TimeoutException {
+    public static void main(String[] args) throws IOException, TimeoutException, InterruptedException {
         ConnectionFactory connectionFactory
                 = new ConnectionFactory();
         connectionFactory.setHost(HOST);
         connectionFactory.setPort(PORT);
+        connectionFactory.setUsername(USERNAME);
+        connectionFactory.setPassword(PASSWORD);
 
         Connection connection = connectionFactory.newConnection();
 
-        transaction(connection);
+//        transaction(connection);
 
         publisherConfirmation(connection);
 
@@ -53,13 +57,14 @@ public class PublishAck {
                 (double) (System.currentTimeMillis() - begin) / 1000, COUNT);
     }
 
-    private static void publisherConfirmation(Connection connection) throws IOException, TimeoutException {
+    private static void publisherConfirmation(Connection connection) throws IOException, TimeoutException, InterruptedException {
         Channel channelA = connection.createChannel();
         channelA.exchangeDeclare(EXCHANGE, "direct");
         channelA.queueDeclare(QUEUE, true, false, true, null);
         channelA.queueBind(QUEUE, EXCHANGE, "test");
         channelA.queuePurge(QUEUE);
 
+        channelA.confirmSelect();
         channelA.addConfirmListener(new ConfirmListener() {
             public void handleAck(long deliveryTag, boolean multiple) throws IOException {
             }
@@ -69,12 +74,11 @@ public class PublishAck {
         });
 
         long begin = System.currentTimeMillis();
-        channelA.confirmSelect();
-
         for (int i = 0; i < COUNT; i++) {
             channelA.basicPublish(EXCHANGE, "test", null, "hello_world".getBytes());
         }
 
+        channelA.waitForConfirmsOrDie();
         System.out.format("It takes %#.2f seconds to publish %d messages with publisher confirmation. %n",
                 (double) (System.currentTimeMillis() - begin) / 1000, COUNT);
 
